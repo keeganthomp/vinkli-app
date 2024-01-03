b89a90121b175ffc0eb383ef509c517ace95aa5d
Keegan Thompson <whereiskeegan@gmail.com>
2024-01-03 08:18:45 -0800

    universal link things

--- app/+html.tsx
+++ app/+html.tsx
@@ -4,10 +4,9 @@ export default function Root({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-
+        {/* For app banner */}
         <meta name="apple-itunes-app" content="app-id=6473663181" />
-
-        {/* Other head elements... */}
+        <meta name="robots" content="noindex" />
       </head>
       <body>{children}</body>
     </html>
